bf3d152fc93fdc870b038d814cad2bb349f39c0c
Pratik Lal <pratila@microsoft.com>
2020-08-22 23:16:36 +0530

    Comments Updated

--- adoclient.py
+++ adoclient.py
@@ -12,11 +12,13 @@ class ADOClient:
     # initializing configparser
     config = configparser.ConfigParser()
     try:
-        config.read(".\\adoaudit.config")
+        config.read(".\\adoaudit.config") # read configuration file.
         logger.AppLogging.auditlogger.info("Reading configuration file adoaudit.config")
-        personal_access_token = config.get("ADO_options", "personal_access_token")
-        organization_url = config.get("ADO_options", "organization_url")
+        personal_access_token = config.get("ADO_options", "personal_access_token") # reading PAT value mentioned in configuration file. This is mandatory field.
+        organization_url = config.get("ADO_options", "organization_url") # reading audit service organization URL mentioned in configuration file. This is mandatory field.
         logger.AppLogging.auditlogger.info("Successfully retrieved PAT & Organization URL " + organization_url)
+    
+    # catching the exception if configuration file is not updated.
     except configparser.NoOptionError as ex:
         logger.AppLogging.auditlogger.error("Unable to retrieve configuration from adoaudit.config")
         logger.AppLogging.auditlogger.error(str(ex))
@@ -24,17 +26,24 @@ class ADOClient:
     @staticmethod
     def query_ado_events():
         try:
+            # Read last processed time of the application. 
+            # This time records the last query time of AzureDevOps audit events by this application to avoid pulling duplicate events.
             start_time_file = open(".//last_processed_time", "r")
             get_start_time = start_time_file.read()
             logger.AppLogging.auditlogger.info("Reading last_processed_time file to get last audit event collection"
                                                " time: " + str(get_start_time))
             start_time_file.close()
-        except FileNotFoundError as ex:
+        except FileNotFoundError as ex: # Catch an exception in case there is some problem with last_processed_time file or it is not found.
             logger.AppLogging.auditlogger.error("File last_processed_time not found. File last_processed_time must "
                                                 "exist ")
             logger.AppLogging.auditlogger.error(str(ex))
         try:
+            # Using BasicAuthentication tto query ADO audit service using PAT. Username and password can also be used in case of service accounts.
+            # Using msrest.authentication module and importing BasicAuthentication class.
             credentials = BasicAuthentication('', ADOClient.personal_access_token)
+
+            # Create a connection to AzureDevOps audit service using OrganizationURL and Credentials (PAT).
+            # Using azure.devops.connection module and Connection class to create connection with AzureDevOps audit service.
             connection = Connection(base_url=ADOClient.organization_url, creds=credentials)
             logger.AppLogging.auditlogger.info("Using BasicAuthentication to Authenticate against ADO "
                                                + str(credentials))
@@ -42,9 +51,12 @@ class ADOClient:
         except AttributeError as ex:
             logger.AppLogging.auditlogger.error(str(ex))
         try:
-            ado_client = connection.clients_v6_0.get_audit_client()
+            ado_client = connection.clients_v6_0.get_audit_client() # AuditClient factory v6.0. Gets the version 6.0 of AuditClient.
             logger.AppLogging.auditlogger.info("Initializing clients_v6_0.get_audit_client() " + str(ado_client))
-            get_audit_response = ado_client.query_log(start_time=get_start_time, skip_aggregation=True)
+
+            # Querying audit logs as per time window mentioned in last_processed_time file.
+            # skip_aggregation=True doesn't aggregate events. Make it False if event aggrigation is required.
+            get_audit_response = ado_client.query_log(start_time=get_start_time, skip_aggregation=True) 
             logger.AppLogging.auditlogger.info("Ready to receive response from server " + str(get_audit_response))
         except NameError as ex:
             logger.AppLogging.auditlogger.error(str(ex))
